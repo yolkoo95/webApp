02f74b13746d8c5e85e133fb479cab074de31f35
schronding's cat <15399018708@163.com>
2020-05-03 11:23:24 +0800

    Changed to markdown

--- leetcode/templates/leetcode/code/leetcode/3Sum/README.md
+++
@@ -1,4 +0,0 @@
-one problem consist of:
-  1. probName.txt and probNameNote.txt in static directory, which
-  is for describing problems and noting for problems.
-  2. code file[cpp.html] in template file.
--- leetcode/views.py
+++ leetcode/views.py
@@ -7,28 +7,21 @@ def codehome(request):
 
     return render(request, 'leetcode/codehome.html')
 
-def codepage(request, category, probName):
+def codepage(request, category, problemName):
     
     # read problem description and note from txt
-    dirPath = os.path.dirname(os.path.abspath(__file__))
-    relPath = 'static/leetcode/src/' + category + '/' + probName + '.txt'
-    relPathNote = 'static/leetcode/src/' + category + '/' + probName + 'Note.txt'
-    fileURL = os.path.join(dirPath, relPath)
-    noteURL = os.path.join(dirPath, relPathNote)
+    # dirPath = os.path.dirname(os.path.abspath(__file__))
+    # relPath = 'static/leetcode/src/' + category + '/' + probName + '.txt'
+    # relPathNote = 'static/leetcode/src/' + category + '/' + probName + 'Note.txt'
+    # fileURL = os.path.join(dirPath, relPath)
     
-    with open(fileURL, 'r') as f:
-        lines = (line.rstrip() for line in f)
-        lines = list((line for line in lines if line))
-        desc1 = lines.pop(0)
-        desc2 = lines.pop(0)
-        begin = lines.pop(0) # example description
-        examples = lines
-    
-    with open(noteURL, 'r') as f:        
-        lines = (line.rstrip() for line in f)
-        notes = list((line for line in lines if line))
-
-    # return (HttpResponse(notes))
+    # with open(fileURL, 'r') as f:
+    #     lines = (line.rstrip() for line in f)
+    #     lines = list((line for line in lines if line))
+    #     desc1 = lines.pop(0)
+    #     desc2 = lines.pop(0)
+    #     begin = lines.pop(0) # example description
+    #     examples = lines
     
     # default language in cs50 is python, we want to add more language with addition argument
     if category == 'leetcode':
@@ -45,15 +38,11 @@ def codepage(request, category, probName):
         'category': category,
         'language': language,
         'addition': addition,
-        'probName': probName,
-        'desc1': desc1, 
-        'desc2': desc2,
-        'begin': begin,
-        'examples': examples,
-        'notes': notes,
-        'cppURL': 'leetcode/code/' + category + '/' + probName + '/cpp.html',
-        'pyURL': 'leetcode/code/' + category + '/' + probName + '/py.html',
-        'htmlURL': 'leetcode/code/' + category + '/' + probName + '/html.html',
+        'problemName': problemName,
+        'problemURL': 'leetcode/code/' + category + '/' + problemName + '/' + problemName + '.html',
+        'cppURL': 'leetcode/code/' + category + '/' + problemName + '/cpp.html',
+        'pyURL': 'leetcode/code/' + category + '/' + problemName + '/py.html',
+        'htmlURL': 'leetcode/code/' + category + '/' + problemName + '/html.html',
     }
     # URL indicate where to find source code 
 
